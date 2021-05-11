18e2b732268926176876172c497e8577a9417b09
Cyell <66208845+Cyell@users.noreply.github.com>
2021-05-11 11:33:34 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Feil.showImage(0)
+})
+input.onButtonPressed(Button.B, function () {
+    Feil.showImage(5)
+})
+let Feil: Image = null
+Feil = images.createBigImage(`
+    . . # . . . . # . .
+    . . # . . . # # # .
+    # . # . # # . # . #
+    . # # # . . . # . .
+    . . # . . . . # . .
+    `)
+basic.forever(function () {
+	
+})
